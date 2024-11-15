07daa37718e3072a1364cc43d2c69c3643cf4393
moses0315 <moses315@naver.com>
2024-11-15 23:50:49 +0900

    Basic SDL, Cmake Settup

---
+++ build/CMakeFiles/MySDL2Project.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MySDL2Project.dir/src/main.cpp.obj"
+  "CMakeFiles/MySDL2Project.dir/src/main.cpp.obj.d"
+  "MySDL2Project.exe"
+  "MySDL2Project.exe.manifest"
+  "MySDL2Project.pdb"
+  "libMySDL2Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MySDL2Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/MySDL2Project.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MySDL2Project.
+# This may be replaced when dependencies are built.
